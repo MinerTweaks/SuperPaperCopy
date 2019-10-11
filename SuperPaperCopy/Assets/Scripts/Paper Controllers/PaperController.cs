3242a017466aac750b1b3fff6993a82fd5bdb54e
MinerTweaks <53493814+MinerTweaks@users.noreply.github.com>
2019-10-11 08:46:44 -0500

    The initial commit
    The basic implementation of the system.

---
+++ SuperPaperCopy/Assets/Scripts/Paper Controllers/PaperController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Streamline this , possibly add features, look into alternate method of calculating , see if there is a way to change camera angle?
+/// </summary>
+public class PaperController : MonoBehaviour {
+    // references
+    [Header( "References" )]
+    public CharacterController controller;
+    public Transform targetDirection;
+    public SpriteRenderer sprRenderer;
+
+    [Header( "Right and left sprites" )]
+    public Sprite facingRightSprite;
+    public Sprite facingLeftSprite;
+
+    [Header( "Character controller stats" )]
+    public float gravity = 20.0f;
+    public float speed = 2;
+    public float jumpForce = 4;
+
+    private Vector3 moveDirection;
+
+    [Header( "Sprite rotation" )]
+    [Tooltip( "The speed at which the sprite rotates" )] public float rotateSpeed = 90;
+
+    [Tooltip( "Max angle the sprite can rotate to" )] public float angle = 60;
+
+    private Vector3 localDirection;
+
+    /// <summary>
+    /// Adds in menu item to create sprite 3d. Character controller and sprites will still need to be set in the inspector!
+    /// </summary>
+    /// <param name="menuCommand"></param>
+    [MenuItem( "GameObject/3D Object/Sprite 3D" , false , 10 )]
+    static void CreateCustomGameObject ( MenuCommand menuCommand ) {
+        // Create a custom game object
+        GameObject go = new GameObject( "Sprite3D" );
+        // Ensure it gets reparented if this was a context click (otherwise does nothing)
+        GameObjectUtility.SetParentAndAlign( go , menuCommand.context as GameObject );
+
+        PaperController pc = go.AddComponent<PaperController>();
+        pc.controller = go.AddComponent<CharacterController>();
+
+        GameObject spr = new GameObject( "Sprite" );
+        spr.transform.SetParent( go.transform , false );
+        pc.sprRenderer = spr.AddComponent<SpriteRenderer>();
+
+        pc.targetDirection = new GameObject("TargetDirection").transform;
+        pc.targetDirection.transform.SetParent( go.transform , false );
+        pc.targetDirection.transform.position = new Vector3( 0 , -0.5f , 0.1f ); 
+
+        // Register the creation in the undo system
+        Undo.RegisterCreatedObjectUndo( go , "Create " + go.name );
+        Selection.activeObject = go;
+    }
+
+
+    private void Update () {
+        Movement();
+        RotateSprite();
+    }
+
+    #region Character controller
+
+    /// <summary>
+    /// Handles the movement of the character controller.
+    /// </summary>
+    private void Movement () {
+        moveDirection.y = moveDirection.y - ( gravity * Time.deltaTime );
+        controller.Move( moveDirection * Time.deltaTime );
+    }
+
+    /// <summary>
+    /// Set the direction the character will move.
+    /// </summary>
+    /// <param name="dir"></param>
+    public void SetMoveDirection ( Vector3 dir ) {
+        if ( controller.isGrounded ) {
+            moveDirection = dir.normalized * speed;
+
+            // rotate the gameobject that indicates the target direction the object is facing
+            if ( moveDirection.x > 0 ) {
+                targetDirection.rotation = Quaternion.LookRotation( moveDirection , transform.up );
+            } else if ( moveDirection.x < 0 ) {
+                // if facing left without this bit , the target direction won't be oriented properly
+                targetDirection.rotation = Quaternion.LookRotation( -moveDirection , transform.up );
+            }
+        }
+    }
+
+    /// <summary>
+    /// Send a jump command if able
+    /// </summary>
+    public void Jump () {
+        if ( controller.isGrounded == true )
+            moveDirection.y = jumpForce;
+    }
+
+
+    #endregion character controller
+
+    /// <summary>
+    /// Rotates the sprite in the direction of character movement
+    /// </summary>
+    private void RotateSprite () {
+        // might not be needed?
+        // localDirection = -targetDirection.right;
+
+        // set the sprite to face the right direction based off of where the character is moving
+        if ( moveDirection.x != 0 )
+            sprRenderer.sprite = moveDirection.x > 0 ? facingRightSprite : facingLeftSprite;
+
+        // rotate the vector3 towards the target direction with the angle clamping the max rotation
+        localDirection = Vector3.RotateTowards( Vector3.forward , -targetDirection.right , Mathf.Deg2Rad * angle , float.MaxValue );
+
+        // apply the rotation
+        sprRenderer.transform.localRotation = Quaternion.RotateTowards( sprRenderer.transform.localRotation , Quaternion.LookRotation( localDirection ) , rotateSpeed * Time.deltaTime );
+    }
+}
---
+++ SuperPaperCopy/Assets/Scripts/Paper Controllers/UserInput.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UserInput : MonoBehaviour {
+
+    public List<PaperController> selectedUnits;
+
+    public PaperController target;
+
+    public float ignoreMovementThreshold = 0.15f;
+
+    private void Update () {
+        Vector3 moveDirection = new Vector3( Input.GetAxis( "Horizontal" ) , 0.0f , Input.GetAxis( "Vertical" ) );
+        // add in a deadzone
+        if(moveDirection.magnitude >= ignoreMovementThreshold || moveDirection.magnitude == 0)
+            target.SetMoveDirection( moveDirection );   
+
+        if ( Input.GetKeyUp( KeyCode.Space ) ) {
+            target.Jump();
+        }
+    }
+
+    private void GetLeftClick () {
+        // basic left click will add a unit to the selection
+
+        if ( Input.GetMouseButtonUp( 0 ) ) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            if ( Physics.Raycast( ray , out hit , 10000.0f ) ) {
+                //if ( hit.collider.GetComponentInChildren<ISelectable>() != null ) {
+
+                    Debug.Log( "You selected the " + hit.transform.name );
+
+                    //hit.collider.GetComponentInChildren<ISelectable>().OnSelect();
+
+                    /*if ( selectedUnits.Contains( hit.collider.GetComponent<Character>() ) == false )
+                        selectedUnits.Add( hit.collider.GetComponent<Character>() );*/
+               // }
+            }
+        }
+
+
+    }
+
+    private void GetRightClick () {
+        if ( Input.GetMouseButtonUp( 1 ) ) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            if ( Physics.Raycast( ray , out hit , 10000.0f ) ) {
+                foreach ( var item in selectedUnits ) {
+                    Debug.DrawLine( hit.point , item.transform.position , Color.black , 5.0f );
+                    //item.SetCommand( new TravelCommand( item , hit.point ) );
+                    //item.SetDestination( hit.point );
+                }
+
+                //Debug.Log( "You selected the " + hit.transform.name );
+            }
+        }
+    }
+
+    private Vector3 GetMousePositionHit () {
+        if ( Input.GetKeyUp( KeyCode.Z ) ) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            if ( Physics.Raycast( ray , out hit , 500.0f ) ) {
+                return hit.point;
+            }
+        }
+        return new Vector3();
+    }
+
+
+    private Vector3 GetMousePosition () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        if ( Physics.Raycast( ray , out hit , 500.0f ) ) {
+            return hit.point;
+        }
+        return new Vector3();
+        /*
+        Vector3 pos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+        pos.z = 0;
+
+        return pos;*/
+    }
+
+
+    private PaperController GetActorUnderMouse () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        
+        if ( Physics.Raycast( ray , out hit , 10000.0f ) ) {
+            if ( hit.transform.GetComponent<Collider>() != null && hit.transform.parent.GetComponent<PaperController>() ) 
+                return hit.transform.parent.GetComponent<PaperController>();
+        }
+
+        return null;
+    }
+}
