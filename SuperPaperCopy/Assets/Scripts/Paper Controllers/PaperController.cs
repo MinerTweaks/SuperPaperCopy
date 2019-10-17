b05e84a5ef90c10633792eae3d701f2a0328f05e
MinerTweaks <53493814+MinerTweaks@users.noreply.github.com>
2019-10-17 13:32:12 -0500

    0.1
    Cleaned up alot of the project , streamlined the code and remove bloat.

--- SuperPaperCopy/Assets/Scripts/Paper Controllers/PaperController.cs
+++ SuperPaperCopy/Assets/Scripts/Paper Controllers/PaperController.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 using UnityEditor;
 
 /// <summary>
-/// Streamline this , possibly add features, look into alternate method of calculating , see if there is a way to change camera angle?
+/// A base implementation of a super paper mario style sprite view using the unity's built in character controller. Input is handled in the UserInput class , to seperate the
+/// input allowing for enemies to use it.
+/// SIDE NOTE - This will only work with a default camera angle, which really sucks and I am looking for a way to fix this.
 /// </summary>
 public class PaperController : MonoBehaviour {
-    // references
     [Header( "References" )]
-    public CharacterController controller;
-    public Transform targetDirection;
-    public SpriteRenderer sprRenderer;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private SpriteRenderer sprRenderer;
 
     [Header( "Right and left sprites" )]
     public Sprite facingRightSprite;
@@ -23,24 +23,22 @@ public class PaperController : MonoBehaviour {
     public float speed = 2;
     public float jumpForce = 4;
 
-    private Vector3 moveDirection;
 
     [Header( "Sprite rotation" )]
-    [Tooltip( "The speed at which the sprite rotates" )] public float rotateSpeed = 90;
+    [Tooltip( "The speed at which the sprite rotates" )] public float rotateSpeed = 120;
+    [Tooltip( "Max angle the sprite can rotate to" )] public float angle = 45;
 
-    [Tooltip( "Max angle the sprite can rotate to" )] public float angle = 60;
-
+    private Vector3 moveDirection;
+    private Quaternion targetRotation;
     private Vector3 localDirection;
 
     /// <summary>
-    /// Adds in menu item to create sprite 3d. Character controller and sprites will still need to be set in the inspector!
+    /// Adds in menu item to create sprite 3d. Sprites will still need to be set in the inspector!
     /// </summary>
     /// <param name="menuCommand"></param>
     [MenuItem( "GameObject/3D Object/Sprite 3D" , false , 10 )]
     static void CreateCustomGameObject ( MenuCommand menuCommand ) {
-        // Create a custom game object
         GameObject go = new GameObject( "Sprite3D" );
-        // Ensure it gets reparented if this was a context click (otherwise does nothing)
         GameObjectUtility.SetParentAndAlign( go , menuCommand.context as GameObject );
 
         PaperController pc = go.AddComponent<PaperController>();
@@ -49,19 +47,21 @@ public class PaperController : MonoBehaviour {
         GameObject spr = new GameObject( "Sprite" );
         spr.transform.SetParent( go.transform , false );
         pc.sprRenderer = spr.AddComponent<SpriteRenderer>();
+        pc.sprRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        pc.sprRenderer.receiveShadows = true;
 
-        pc.targetDirection = new GameObject("TargetDirection").transform;
-        pc.targetDirection.transform.SetParent( go.transform , false );
-        pc.targetDirection.transform.position = new Vector3( 0 , -0.5f , 0.1f ); 
+        Debug.LogWarning("Set the sprites material to the SpriteLit material in materials folder!");
 
-        // Register the creation in the undo system
         Undo.RegisterCreatedObjectUndo( go , "Create " + go.name );
         Selection.activeObject = go;
     }
 
 
     private void Update () {
+        // transform.GetChild( 0 ).transform.rotation = Quaternion.Euler(new Vector3( 0,Camera.main.transform.rotation.eulerAngles.y,0 )); Ignore me attempting to align with camera view using second gameobject
+
         Movement();
+        SetSpriteState();
         RotateSprite();
     }
 
@@ -85,16 +85,16 @@ public class PaperController : MonoBehaviour {
 
             // rotate the gameobject that indicates the target direction the object is facing
             if ( moveDirection.x > 0 ) {
-                targetDirection.rotation = Quaternion.LookRotation( moveDirection , transform.up );
+                targetRotation = Quaternion.LookRotation( moveDirection , transform.up );
             } else if ( moveDirection.x < 0 ) {
                 // if facing left without this bit , the target direction won't be oriented properly
-                targetDirection.rotation = Quaternion.LookRotation( -moveDirection , transform.up );
+                targetRotation = Quaternion.LookRotation( -moveDirection , transform.up );
             }
         }
     }
 
     /// <summary>
-    /// Send a jump command if able
+    /// Set the y value of move direction to perform a jump but only if we are grounded
     /// </summary>
     public void Jump () {
         if ( controller.isGrounded == true )
@@ -103,21 +103,22 @@ public class PaperController : MonoBehaviour {
 
 
     #endregion character controller
-
     /// <summary>
-    /// Rotates the sprite in the direction of character movement
+    /// Set the sprite on the spriterenderer based on the x movement
     /// </summary>
-    private void RotateSprite () {
-        // might not be needed?
-        // localDirection = -targetDirection.right;
-
-        // set the sprite to face the right direction based off of where the character is moving
+    private void SetSpriteState () {
         if ( moveDirection.x != 0 )
             sprRenderer.sprite = moveDirection.x > 0 ? facingRightSprite : facingLeftSprite;
+    }
 
+    /// <summary>
+    /// Rotates the sprite in the direction of character movement - This is the important part of the code, the rest is pretty irrelevant.
+    /// This takes the targetRotation variable and rotates the localDirection towards it , not exceeding the max angle , then sets the sprites rotation to the value recieved.
+    /// </summary>
+    private void RotateSprite () {
         // rotate the vector3 towards the target direction with the angle clamping the max rotation
-        localDirection = Vector3.RotateTowards( Vector3.forward , -targetDirection.right , Mathf.Deg2Rad * angle , float.MaxValue );
-
+        localDirection = Vector3.RotateTowards( Vector3.forward , targetRotation * -transform.right , Mathf.Deg2Rad * angle , float.MaxValue );
+        Debug.DrawRay(transform.position , -Camera.main.transform.right);
         // apply the rotation
         sprRenderer.transform.localRotation = Quaternion.RotateTowards( sprRenderer.transform.localRotation , Quaternion.LookRotation( localDirection ) , rotateSpeed * Time.deltaTime );
     }
